3c46d801333924b6168c4f08b215c1527dd3ce34
Supawich Sitdhipol <Supawichs@gmail.com>
2022-04-10 15:30:17 +0700

    Add database user model

---
+++ backend/app/models/post.py
@@ -0,0 +1,18 @@
+from uuid import uuid4
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import  Column, String, Date, Float
+from sqlalchemy.orm import relationship
+from ..database.init_db import Base
+
+
+class Client(Base):
+    __tablename__ = "user"
+
+    user_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    name = Column(String(100))
+    user_score = Column(Float)
+    update_score_date = Column(Date)
+    profile_picture_url = Column(String(2048))
+    
+
+    # pets = relationship("Pet", back_populates="owner")
