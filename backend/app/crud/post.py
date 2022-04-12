8fb0d358f3d120d0c62c860ebc83bd5d5be9a36a
Palm <supawichs@gmail.com>
2022-04-13 01:57:24 +0700

    Add post crud functions

--- backend/app/crud/post.py
+++ backend/app/crud/post.py
@@ -1,7 +1,7 @@
 from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
 from backend.app.models.post import Post
-from backend.app.schemas.post import PostBase, PostEditScore, PostEditProfile
+from backend.app.schemas.post import PostBase, PostEditScore, PostEditData
 
 def create_post(request: PostBase, db:Session):
     pass
@@ -12,3 +12,12 @@ def get_post_by_id(id, db:Session):
 def get_post_score_by_id(id, db:Session):
     pass
 
+def update_post_score_by_id(id, request:PostEditScore, db:Session):
+    pass
+
+def update_post_data_by_id(id, request:PostEditData, db:Session):
+    pass
+
+
+def del_post_by_id(id, db:Session):
+    pass
