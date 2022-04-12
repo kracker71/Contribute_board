015085f668aa184200c7ba3d2180d9ed450e5217
Palm <supawichs@gmail.com>
2022-04-13 01:15:52 +0700

    Add post schema

---
+++ backend/app/schemas/post.py
@@ -0,0 +1,22 @@
+from typing import List, Optional
+from datetime import date
+from pydantic import BaseModel
+
+class PostBase(BaseModel):
+    post_id: str
+    post_data: str
+    post_url: str
+    date_scrape: date
+    date_post: date
+    post_score: float
+    post_likes: int
+    
+class PostEditData(BaseModel):
+    post_data: str
+    date_scrape: date
+
+class PostEditScore(BaseModel):
+    psot_score: float
+    
+    
+    
