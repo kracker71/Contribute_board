f32ee2284327abfe0acaa597dcd488c78a365087
Bxntn <nuttanan118123@gmail.com>
2022-04-13 18:26:53 +0700

    Fix date type and Add ShowUser

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -4,16 +4,25 @@ from pydantic import BaseModel
 
 #update_score_base
 class UserBase(BaseModel):
+    profile_url :str 
+    
+class UserRegister(UserBase):
     profile_picture_url:str
     name:str
     user_score:Optional[float] = 0    
-    user_score_date:Optional[date] = 0    
+    update_score_date:date
     
 class UserEditScore(BaseModel):
     user_score:float
-    update_score_date:int
+    update_score_date:date
     
 class UserEditProfile(BaseModel):
     profile_picture_url:str
     name:str
     
+class ShowUser(BaseModel):
+    name:str
+    user_score:Optional[float] = 0 
+    update_score_date:date
+    class Config:
+        orm_mode = True
