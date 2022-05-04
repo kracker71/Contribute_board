b8991856f7465d5cd8a82bb4b042b74aa7200b9e
p4ssa <chokunsmurf2@gmail.com>
2022-05-04 19:13:29 +0700

    feat: edit leaderboard title
    - change font weight to each font family
    - edit leaderboard title text
    - edit leaderboard title style

--- frontend/src/Components/LeaderboardTitle/LeaderboardTitle.js
+++ frontend/src/Components/LeaderboardTitle/LeaderboardTitle.js
@@ -6,18 +6,19 @@ class LeaderboardTitle extends Component {
   render() {
     return (
       <div className="LeaderboardTitle">
-        <p className="title">Contributor Ranking</p>
         <p className="facebookGroup">
-          Poker Thailand Community
+          โป๊กเกอร์ Thailand
           <a
             className="groupLink"
-            href="https://www.facebook.com/groups/223422232142743/"
+            href="https://www.facebook.com/groups/291396973145616"
             target="_blank"
             rel="noreferrer"
           >
-            <img className="logo" src={fbLogo} alt="fbLogo"></img>
+            <img className="logo" src={fbLogo} alt="facebook-logo"></img>
           </a>
         </p>
+        <p className="title">Contributor Ranking</p>
+        <p className="sub-title">ตารางจัดอันดับผู้มีส่วนร่วมในกลุ่ม</p>
       </div>
     );
   }
