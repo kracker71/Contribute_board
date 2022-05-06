0f7199608d7aa8e4adcf3288aa1cab291f064ac1
p4ssa <chokunsmurf2@gmail.com>
2022-05-06 16:07:10 +0700

    feat: change top contributor list style

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div>
         {/* <NavigationBar /> */}
         <LeaderboardTitle />
-        {/* <Leaderboard /> */}
+        <Leaderboard />
       </div>
     );
   }
--- frontend/src/Components/TopContributor/TopContributor.js
+++ frontend/src/Components/TopContributor/TopContributor.js
@@ -5,14 +5,14 @@ class TopContributor extends Component {
   render() {
     return (
       <div className={"TopContributor " + this.props.place}>
-        <p className="rank"># {this.props.info.rank}</p>
         <img
           className="profile rounded-circle"
           src={this.props.info.image}
           alt={this.props.info.name}
         />
+        <p className="rank">{this.props.info.rank}</p>
         <p className="name">{this.props.info.name}</p>
-        <p className="point">{this.props.info.point} points</p>
+        <p className="point">{this.props.info.point}</p>
       </div>
     );
   }
