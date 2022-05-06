608830c867a2a4907526d7f14916a7d242c4fc95
p4ssa <chokunsmurf2@gmail.com>
2022-05-06 16:39:09 +0700

    feat: add border to profile image

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -12,7 +12,7 @@ class App extends Component {
           <div className="background-filter">
             {/* <NavigationBar /> */}
             <LeaderboardTitle />
-            {/* <Leaderboard /> */}
+            <Leaderboard />
           </div>
         </div>
       </div>
