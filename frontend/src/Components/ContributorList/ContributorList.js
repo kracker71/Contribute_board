4307dbceaf722d212a2354b9fdfb1d5f390853a8
Chavalvit Keartnattakorn <cho17872@hotmail.com>
2022-05-23 13:02:17 +0700

    refact: add key prop to contributor list
    - add key prop to contributor list
    - delete unuse import Col and Row

--- frontend/src/Components/ContributorList/ContributorList.js
+++ frontend/src/Components/ContributorList/ContributorList.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import "./ContributorList.css";
 import Contributor from "../Contributor/Contributor.js";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 class ContributorList extends Component {
   render() {
     const contributorList = this.props.contributor.map((contributor) => {
-      return <Contributor info={contributor} />;
+      return <Contributor info={contributor} key={contributor.rank} />;
     });
     return (
       <div className="ContributorList">
